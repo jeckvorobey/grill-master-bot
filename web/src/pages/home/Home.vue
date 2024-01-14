f7d082b75c95749ffeccd25f56997ba57b5c93f1
jeckvorobey <jeckvorobey69@gmail.com>
2024-01-14 18:45:21 +0300

    app web interface

---
+++ web/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-card flat square class="q-pa-md" style="height: 100vh">
+    <q-toolbar-title class="q-pa-md text-center text-h4 bg-accent">Меню</q-toolbar-title>
+
+    <q-card-section>
+
+    </q-card-section>
+
+    <!--  // Блок с меню-->
+  </q-card>
+
+</template>
+
+<script setup lang="ts">
+//
+</script>
