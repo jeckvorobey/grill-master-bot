89ac80eb0fbdfbd35c3c1efcc8d05f278cd98dac
serg <jeckvorobey69@gmail.com>
2024-01-28 19:02:55 +0300

    save change

--- tgbot/handlers/onboarding/static_text.py
+++ tgbot/handlers/onboarding/static_text.py
@@ -1,7 +1,8 @@
 start_created = "Sup, {first_name}!"
 start_not_created = "Welcome back, {first_name}!"
-unlock_secret_room = "Congratulations! You've opened a secret room👁‍🗨. There is some information for you:\n" \
+unlock_secret_room = "Поздравляем! Вы открыли секретную комнату👁‍🗨. Есть некоторая информация для вас:\n" \
            "<b>Users</b>: {user_count}\n" \
            "<b>24h active</b>: {active_24}"
 github_button_text = "GitHub"
 secret_level_button_text = "Secret level🗝"
+menu_text = "Меню"
