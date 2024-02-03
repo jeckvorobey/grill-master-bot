ff6b6489428bcbd1ad7b075b18ab574586550cfb
serg <jeckvorobey69@gmail.com>
2024-02-03 13:12:53 +0300

    fix files

--- web/src/types/components.d.ts
+++ web/src/types/components.d.ts
@@ -7,7 +7,8 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    FoodListComponent: typeof import('./../src/components/FoodListComponent.vue')['default']
+    FoodListComponent: typeof import('../components/FoodListComponent.vue')['default']
+    HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
